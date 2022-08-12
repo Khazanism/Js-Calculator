9e8df844db1204bedfe07e129852c91369ff30f1
Khazanism <khazanyassin@gmail.com>
2022-08-12 21:57:19 +0200

    all const added to logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,13 @@
 //  Calculator
 
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const equalsButtons = document.querySelector("[data-equals]");
+const deleteButtons = document.querySelector("[data-delete]");
+const allClearButtons = document.querySelector("[data-all-clear]");
+const previousOperandTextElement = document.querySelector(
+  "[data-previous-operand]"
+);
+const currentOperandTextElement = document.querySelector(
+  "[data-current-operand]"
+);
