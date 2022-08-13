422e4c14f3644adbea22bf04bfef60b2efab93b8
Khazanism <khazanyassin@gmail.com>
2022-08-13 18:13:49 +0200

    calc functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,26 +5,33 @@ class Calculator {
     this.currentOperandTextElement = currentOperandTextElement;
   }
 }
-
+//  clear out our different variables 
 clear() {
-
+  this.currentOpeerand = ''
+  this.previousOperand = ''
+  this.operation = undefined
 }
-
+// removing a single number 
 delete() {
 
 }
-
+// add a number every time iclick on a number to add to screen
 appendNumber(number) {
 
 }
-
+// every time the op is clicked it picks the op the user selected
 chooseOperation(operation) {
 
 }
-
+// take our values inside our calc. and compute a single value
+// for what we need to display.
 compute() {
 
 } 
+// update the vaues inside our output
+updateDisplay(){
+
+}
 
 const numberButtons = document.querySelectorAll("[data-number]");
 const operationButtons = document.querySelectorAll("[data-operation]");
