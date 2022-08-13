7d0285e71f339f4123d6310f100563b93f810ecd
Khazanism <khazanyassin@gmail.com>
2022-08-13 17:48:23 +0200

    understanding logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,30 @@
 //  Calculator
+class Calculator {
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement;
+    this.currentOperandTextElement = currentOperandTextElement;
+  }
+}
+
+clear() {
+
+}
+
+delete() {
+
+}
+
+appendNumber(number) {
+
+}
+
+chooseOperation(operation) {
+
+}
+
+compute() {
+
+} 
 
 const numberButtons = document.querySelectorAll("[data-number]");
 const operationButtons = document.querySelectorAll("[data-operation]");
