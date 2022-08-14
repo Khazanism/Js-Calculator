c44a860cef46dce1af7802b9ca616ac582c8ac08
Khazanism <khazanyassin@gmail.com>
2022-08-14 12:21:54 +0200

    numberbuttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,33 +4,35 @@ class Calculator {
     this.previousOperandTextElement = previousOperandTextElement;
     this.currentOperandTextElement = currentOperandTextElement;
   }
-}
-//  clear out our different variables 
-clear() {
-  this.currentOpeerand = ''
-  this.previousOperand = ''
-  this.operation = undefined
-}
-// removing a single number 
-delete() {
 
-}
-// add a number every time iclick on a number to add to screen
-appendNumber(number) {
+  //  clear out our different variables
+  clear() {
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.operation = undefined;
+  }
 
-}
-// every time the op is clicked it picks the op the user selected
-chooseOperation(operation) {
+  // removing a single number
+  delete() {}
 
-}
-// take our values inside our calc. and compute a single value
-// for what we need to display.
-compute() {
+  // add a number every time iclick on a number to add to screen
+  appendNumber(number) {
+    this.currentOperand = number;
+  }
 
-} 
-// update the vaues inside our output
-updateDisplay(){
+  // every time the op is clicked it picks the op the user selected
+  chooseOperation(operation) {
+    this.currentOperand = number;
+  }
 
+  // take our values inside our calc. and compute a single value
+  // for what we need to display.
+  compute() {}
+
+  // update the vaues inside our output
+  updateDisplay() {
+    this.currentOperandTextElement.innerText = this.currentOperand;
+  }
 }
 
 const numberButtons = document.querySelectorAll("[data-number]");
@@ -44,3 +46,15 @@ const previousOperandTextElement = document.querySelector(
 const currentOperandTextElement = document.querySelector(
   "[data-current-operand]"
 );
+
+const Calculator = new Calculator(
+  previousOperandTextElement,
+  currentOperandTextElement
+);
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    Calculator.appendNumber(button.innerText);
+    Calculator.updateDisplay();
+  });
+});
